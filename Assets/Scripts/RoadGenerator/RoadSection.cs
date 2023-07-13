aed4b093133f90ba4fa0132b2ba947f16ab7cbf0
Johnny Howe <jonathon.o.howe@gmail.com>
2023-07-14 11:38:51 +1200

    Road section reference to bounding mesh (filter)

--- Assets/Scripts/RoadGenerator/RoadSection.cs
+++ Assets/Scripts/RoadGenerator/RoadSection.cs
@@ -6,16 +6,7 @@ namespace RoadGeneration
 {
     public class RoadSection : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        [SerializeField] private MeshFilter _boundingMesh;
+        
     }
 }
