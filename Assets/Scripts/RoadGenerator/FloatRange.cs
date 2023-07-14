e775c6ebf3e1c82f717dd8ec6729857d7876ddcb
Johnny Howe <jonathon.o.howe@gmail.com>
2023-07-14 14:57:29 +1200

    FloatRange equality override and basic test

--- Assets/Scripts/RoadGenerator/FloatRange.cs
+++ Assets/Scripts/RoadGenerator/FloatRange.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace RoadGeneration
 {
-    public class FloatRange
+    public class FloatRange : IEquatable<FloatRange>
     {
         public float Min { get; private set; }
         public float Max { get; private set; }
@@ -17,5 +17,32 @@ namespace RoadGeneration
             Min = Math.Min(value1, value2);
             Max = Math.Max(value1, value2);
         }
+
+        public bool Equals(FloatRange other)
+        {
+            return other.Min == Min && other.Max == Max;
+        }
+
+        public override bool Equals(object obj) => this.Equals(obj as FloatRange);
+
+        public override int GetHashCode() => (Min, Max).GetHashCode();
+
+        public static bool operator ==(FloatRange lhs, FloatRange rhs)
+        {
+            if (lhs is null)
+            {
+                if (rhs is null)
+                {
+                    return true;
+                }
+
+                // Only the left side is null.
+                return false;
+            }
+            // Equals handles case of null on right side.
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(FloatRange lhs, FloatRange rhs) => !(lhs == rhs);
     }
 }
