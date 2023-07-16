c3c25196fb27abfe130aea060d69aa70c281bd3f
Johnny Howe <jonathon.o.howe@gmail.com>
2023-07-16 13:52:35 +1200

    Stripped everything that implied the old structure

--- Assets/Scripts/RoadGenerator/IRoadSection.cs
+++ Assets/Scripts/RoadGenerator/IRoadSection.cs
@@ -5,18 +5,5 @@ namespace RoadGeneration
     public interface IRoadSection
     {
         RoadSectionShape GetLocalShape();
-
-        RoadSectionBounds GetGlobalBounds();
-        Vector3 GetGlobalEndPosition();
-        Vector3 GetLocalStartPosition();
-        Quaternion GetGlobalEndRotation();
-        Quaternion GetLocalStartRotation();
-
-        /// <summary>
-        /// Get a COPY of this aligned such that it is aligned with the start of this at
-        /// startPosition and with a rotation about the y axis of yAxisRotation (zero on 
-        /// other axes)
-        /// </summary>
-        IRoadSection GetAlignedTo(Vector3 startPosition, float yAxisRotationStart);
     }
 }
--- Assets/Scripts/RoadGenerator/RoadGenerator.cs
+++ Assets/Scripts/RoadGenerator/RoadGenerator.cs
@@ -8,28 +8,5 @@ namespace RoadGeneration
 {
     public class RoadGenerator : MonoBehaviour
     {
-        [SerializeField] private List<RoadSection> _cornerPrefabs;  // Cannot serialize IRoadSection for inspector, so this will do
-        [SerializeField] private RoadGeneratorChoiceEngine _roadGeneratorEngine;
-        [SerializeField] private int _piecesToGenerate = 10;
-
-        private void Start()
-        {
-            _roadGeneratorEngine = new RoadGeneratorChoiceEngine();
-            _roadGeneratorEngine.UpdateInputsAndResetSearch(new List<IRoadSection>(), _cornerPrefabs.Cast<IRoadSection>().ToList(), 10);
-
-            for (int i = 0; i < _piecesToGenerate; i++)
-            {
-                while (!_roadGeneratorEngine.FoundValidChoice())
-                {
-                    _roadGeneratorEngine.Run(1);
-                }
-                _InstantiateNewPiece(_roadGeneratorEngine.GetNextChoice());
-            }
-        }
-
-        private void _InstantiateNewPiece(IRoadSection roadSection)
-        {
-            Debug.Log(roadSection);
-        }
     }
 }
