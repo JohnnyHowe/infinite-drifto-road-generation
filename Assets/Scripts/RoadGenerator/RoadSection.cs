6a07157d688434ec393310def2c8debaa7b6503c
Johnny Howe <jonathon.o.howe@gmail.com>
2023-07-17 13:26:47 +1200

    Scene setup for road generation

--- Assets/Scripts/RoadGenerator/RoadSection.cs
+++ Assets/Scripts/RoadGenerator/RoadSection.cs
@@ -6,6 +6,10 @@ namespace RoadGeneration
 {
     public class RoadSection : MonoBehaviour, IRoadSection
     {
+        [SerializeField] private Transform _startPoint;
+        [SerializeField] private Transform _endPoint;
+        [SerializeField] private MeshFilter _boundingMesh;
+
         public RoadSectionShape GetLocalShape()
         {
             throw new System.NotImplementedException();
