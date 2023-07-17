5bc3d498d407511e734ece73956ca0aa7fa17a72
Johnny Howe <jonathon.o.howe@gmail.com>
2023-07-17 12:39:11 +1200

    Overlap detector now uses FloatRange and ConvexShape2D DoesOverlapWith methods

--- Assets/Scripts/OverlapDetector.cs
+++ Assets/Scripts/OverlapDetector.cs
@@ -19,39 +19,12 @@ public class OverlapDetector : MonoBehaviour
         // Overlapping in y axis?
         FloatRange object1RangeY = _GetYRange(object1);
         FloatRange object2RangeY = _GetYRange(object2);
-        if (!_AreOverlapping(object1RangeY, object2RangeY)) return false;
+        if (!object1RangeY.DoesOverlapWith(object2RangeY)) return false;
 
         // Overlapping 2D?
         ConvexShape2D object1Shape = _GetShape2D(object1);
         ConvexShape2D object2Shape = _GetShape2D(object2);
-        return _AreOverlapping(object1Shape, object2Shape);
-    }
-
-    /// <summary>
-    /// Separating axis theorem baby
-    /// </summary>
-    private bool _AreOverlapping(ConvexShape2D shape1, ConvexShape2D shape2)
-    {
-        // for each axis of both objects
-        IEnumerable<Vector2> axes = shape1.GetAxes().Concat(shape2.GetAxes()).Distinct().ToList();
-        foreach (Vector2 axis in axes)
-        {
-            Debug.DrawRay(Vector3.zero, new Vector3(axis.x, 0, axis.y));
-            // get the projection of each object on the axis
-            FloatRange shape1ProjectionRange = shape1.GetProjection(axis);
-            FloatRange shape2ProjectionRange = shape2.GetProjection(axis);
-
-            // if there is a gap, return false - there is no overlap
-            if (!_AreOverlapping(shape1ProjectionRange, shape2ProjectionRange)) return false;
-        }
-
-        // no gap found, must be touching
-        return true;
-    }
-
-    private bool _AreOverlapping(FloatRange range1, FloatRange range2)
-    {
-        return range1.Min < range2.Max && range1.Max > range2.Min;
+        return object1Shape.DoesOverlapWith(object2Shape);
     }
 
     private FloatRange _GetYRange(MeshFilter object1)
