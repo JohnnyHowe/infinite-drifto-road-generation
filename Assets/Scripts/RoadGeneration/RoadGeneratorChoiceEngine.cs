d279090799d4df8caf4702593bc5238475ff4364
Johnny Howe <jonathon.o.howe@gmail.com>
2023-07-17 20:07:32 +1200

    More RoadGeneratorChoiceEngine skeleton 2: electric boogaloo

--- Assets/Scripts/RoadGeneration/RoadGeneratorChoiceEngine.cs
+++ Assets/Scripts/RoadGeneration/RoadGeneratorChoiceEngine.cs
@@ -15,10 +15,14 @@ namespace RoadGeneration
         public class NoChoiceFoundException : Exception { }
 
         private DFSCombinationGenerator _combinationGenerator;
+        private List<IRoadSection> _currentPiecesInWorld;
+        private List<IRoadSection> _candidatePrototypes;
 
         public void Reset(List<IRoadSection> currentPiecesInWorld, List<IRoadSection> possibleChoicesInPreferenceOrder, int checkDepth)
         {
             _combinationGenerator = new DFSCombinationGenerator(possibleChoicesInPreferenceOrder.Count, checkDepth);
+            _currentPiecesInWorld = currentPiecesInWorld;
+            _candidatePrototypes = possibleChoicesInPreferenceOrder;
         }
 
         public void StepUntilChoiceIsFound()
@@ -39,6 +43,29 @@ namespace RoadGeneration
         }
 
         private bool _DoesLastCandidateSectionOverlapWithOthers()
+        {
+            List<RoadSectionShape> allPiecesAligned = _GetCandidatesAndCurrentPiecesInWorldAligned();
+            // TODO can allPiecesAligned.Count ever be zero? If so, prevent it from breaking
+            RoadSectionShape currentCandidate = allPiecesAligned[allPiecesAligned.Count - 1];
+
+            foreach (RoadSectionShape worldRoadSectionShape in allPiecesAligned.Take(allPiecesAligned.Count - 1))
+            {
+                if (currentCandidate.DoesOverlapWith(worldRoadSectionShape)) return true;
+            }
+            return false;
+        }
+
+        private List<RoadSectionShape> _GetCandidatesAndCurrentPiecesInWorldAligned()
+        {
+            return _GetCurrentPiecesInWorldShapes().Concat(_GetAlignedCandidates()).ToList();
+        }
+
+        private List<RoadSectionShape> _GetCurrentPiecesInWorldShapes()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<RoadSectionShape> _GetAlignedCandidates()
         {
             throw new NotImplementedException();
         }
